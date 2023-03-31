6248a0ddcce3d831e9bf0a52b8cda7bb5340ee2d
bestiannn <rebolledocofrebastian@gmail.com>
2023-03-30 21:51:43 -0300

    refactor: Loading page has own component

---
+++ src/components/LoadingPage.jsx
@@ -0,0 +1,11 @@
+const LoadingPage = () => {
+    return (
+        <div className='bg-ctp-crust text-ctp-text'>
+            <div className='min-h-screen container mx-auto grid place-content-center'>
+                <h2 className='text-3xl font-bold'>Loading...</h2>
+            </div>
+        </div>
+    )
+}
+
+export default LoadingPage
