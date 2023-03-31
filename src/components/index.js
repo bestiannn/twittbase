828965583d56c66353b4c6e6c15b14e6e8902274
bestiannn <rebolledocofrebastian@gmail.com>
2023-03-30 22:16:27 -0300

    refactor: barrel file for components

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import Home from './Home';
+import Login from './Login';
+import NavBar from './NavBar';
+import Profile from './Profile';
+import Register from './Register';
+import Search from './Search';
+import LoadingPage from './LoadingPage';
+
+export {
+    Home,
+    Login,
+    NavBar,
+    Profile,
+    Register,
+    Search,
+    LoadingPage
+};
